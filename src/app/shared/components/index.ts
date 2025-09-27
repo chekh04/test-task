5e51338e8111187d48b7cc02951a03b9b7033019
sashachekh <dihsdu@gmail.com>
2025-09-27 23:32:37 +0300

    removed unused

--- src/app/shared/components/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './text-input/text-input.component';
-export * from './datepicker/datepicker.component';
-export * from './country-input/country-input.component';
-export * from './dynamic-form/dynamic-form.component';
--- src/app/shared/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './directives';
-export * from './validators';
-export * from './components';
-export * from './enum/country';
-export * from './interface/requests';
-export * from './interface/responses';
